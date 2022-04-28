00c10a007ab9ac619b5f8dc85fed3bc1d35b6179
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-04-28 11:06:30 -0300

    Estudando diferentes formas de declarar funcoes no JS

--- FilmesFavoritos/JS/funcoes.js
+++ FilmesFavoritos/JS/funcoes.js
@@ -1,14 +1,36 @@
-let a = Math.floor(Math.random() * 1000)
+// Diferentes modos de declarar funcoes
 
-function minhaPrimeiraFuncao(){
-    let b = Math.floor(Math.random() * 1000)
-    // return `${a} + ${b} = ${a + b}`
-    if (a > b){
-        return `${a} - ${b} = ${a - b}`
-    }
-    else{
-        return `${a} + ${b} = ${a + b}`
-    }
-}
+// Primeira forma:
+// let a = Math.floor(Math.random() * 1000)
 
-console.log(minhaPrimeiraFuncao())
+// function minhaPrimeiraFuncao(){
+//     let b = Math.floor(Math.random() * 1000)
+//        //return `${a} + ${b} = ${a + b}`
+//     if (a > b){
+//         return `${a} - ${b} = ${a - b}`
+//     }
+//     else{
+//         return `${a} + ${b} = ${a + b}`
+//     }
+// }
+
+// console.log(minhaPrimeiraFuncao())
+
+//Segunda forma:
+
+// Funcao Anonima
+// A funcao nao tem nome
+// Atribuindo uma funcao a uma constante (apenas funcoes anonimas e arrow function)
+// const minhaSegundaFuncao = function(){
+//     return "Escrevendo minha função anônima"
+// }
+// console.log(minhaSegundaFuncao());
+
+//Terceira forma:
+
+// Arrow Function
+// const a = () => {
+//     return "Esta é uma arrow function"
+// }
+// console.log(a());
+
