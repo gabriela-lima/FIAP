49e0534956667001dc88cee023cbebb028e67f5c
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-04-28 15:12:21 -0300

    Adicionando imagens e os titulos de filmes

---
+++ FilmesFavoritos/JS/index.js
@@ -0,0 +1,18 @@
+
+let listaDeFilmes = [
+    'https://br.web.img3.acsta.net/pictures/14/08/01/15/44/388856.jpg',
+    'https://cinemaemcena.com.br/uploads/criticas_old/interstellar_poster5.jpg',
+    'https://br.web.img3.acsta.net/pictures/210/140/21014024_20130619225537406.jpg',
+    'https://ingresso-a.akamaihd.net/img/cinema/cartaz/6429-cartaz.jpg',
+    'https://upload.wikimedia.org/wikipedia/pt/thumb/4/43/Butterflyeffect_poster.jpg/240px-Butterflyeffect_poster.jpg',
+    'https://upload.wikimedia.org/wikipedia/pt/d/d4/Ready_Player_One_%28filme%29.png'
+]
+
+let listaDeTitulos = [
+    'Relatos Selvagens',
+    'Interestelar',
+    'Truque de Mestre',
+    'Cantando na Chuva',
+    'Efeito Borboleta',
+    'Jogador Nº 1'
+]
