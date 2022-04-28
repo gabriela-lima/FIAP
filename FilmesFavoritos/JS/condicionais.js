b2f4d1ac23b2da220ae0dd004dab7992e5bfc1a1
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-04-28 13:57:53 -0300

    Estudando condicionais

---
+++ FilmesFavoritos/JS/condicionais.js
@@ -0,0 +1,11 @@
+let a = Math.floor(Math.random() * 100)
+console.log(a)
+if(a > 50){
+    console.log("O valor de a está na metade maior da sequencia")
+}
+else if(a === 50){
+    console.log("O valor de a é igual a 50")
+}
+else{
+    console.log("O valor de a está na metade menor da sequencia")
+}
