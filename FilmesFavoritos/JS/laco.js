9d75cb3685e9cfa04a178b9769bfab60a93a4e0e
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-04-28 13:44:50 -0300

    Estudando laços de repetição!

--- FilmesFavoritos/JS/laco.js
+++ FilmesFavoritos/JS/laco.js
@@ -1,11 +1,25 @@
 //Lacos de repeticao
 // for, while, do while (principais) (ainda tem o foreach e etc)(entendendo os basicos entende o resto)
 
-// construindo loop para tabuadas
+// for
+// construindo loop for para tabuadas
+// for (let i = 0; i <= 10; i++){
+//     for (let j = 0; j <= 10; j++)
+//     {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+// }
 
-for (let i = 0; i <= 10; i++){
-    for (let j = 0; j <= 10; j++)
-    {
-        console.log(`${i} * ${j} = ${i * j}`);
-    }
-}
+// while
+// let a = 0
+
+// while (a > 0){
+//     console.log("Bom dia do laco while!")
+//     a++
+// }
+
+// // do while
+// // executa uma vez antes de checar a condicao
+// do {
+//     console.log("Bom dia do laco do while!");
+// } while(a > 0)
