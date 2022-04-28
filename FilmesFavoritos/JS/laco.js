dc3a9986b8f60d8d81f294d24bae72779dcbb817
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-04-28 11:44:55 -0300

    Fazendo tabuada com laço de repetição for

---
+++ FilmesFavoritos/JS/laco.js
@@ -0,0 +1,11 @@
+//Lacos de repeticao
+// for, while, do while (principais) (ainda tem o foreach e etc)(entendendo os basicos entende o resto)
+
+// construindo loop para tabuadas
+
+for (let i = 0; i <= 10; i++){
+    for (let j = 0; j <= 10; j++)
+    {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
