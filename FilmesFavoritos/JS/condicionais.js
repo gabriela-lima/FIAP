5f1185823007306ab47027ead2cefcdd5d27931c
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-04-28 14:14:37 -0300

    Diferença entre o == e ===

--- FilmesFavoritos/JS/condicionais.js
+++ FilmesFavoritos/JS/condicionais.js
@@ -1,11 +1,29 @@
-let a = Math.floor(Math.random() * 100)
-console.log(a)
-if(a > 50){
-    console.log("O valor de a está na metade maior da sequencia")
-}
-else if(a === 50){
-    console.log("O valor de a é igual a 50")
-}
-else{
-    console.log("O valor de a está na metade menor da sequencia")
-}
+// let a = Math.floor(Math.random() * 100)
+// console.log(a)
+// if(a > 50){
+//     console.log("O valor de a está na metade maior da sequencia")
+// }
+// else if(a === 50){
+//     console.log("O valor de a é igual a 50")
+// }
+// else{
+//     console.log("O valor de a está na metade menor da sequencia")
+// }
+
+// Diferenca entre a comparacao com dois iguais (==) e tres iguais (===)
+
+let a = 50
+let b = 50
+let c = '50'
+
+// Comparando com dois iguais
+// Apenas compara valores 
+// !=
+// console.log(`Essa comparação é: ${a == b}, ou seja ${a} = ${b}`);
+// console.log(`Essa comparação é: ${a == c}, ou seja ${a} = ${c}`);
+
+// Comparando com tres iguais
+// comparacao de valores e tipos 
+// !==
+// console.log(`Essa comparação é: ${a === b}, ou seja ${a} = ${b}`);
+// console.log(`Essa comparação é: ${a === c}, ou seja ${a} !== ${c}`);
